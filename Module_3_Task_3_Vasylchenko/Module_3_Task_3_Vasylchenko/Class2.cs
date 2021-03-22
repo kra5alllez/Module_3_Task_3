cff2fcdd2ef064eee4c42c22905f1bb75b5ba407
kra5alllez <kra5alllez@gmail.com>
2021-03-22 15:51:31 +0200

    add project

---
+++ Module_3_Task_3_Vasylchenko/Module_3_Task_3_Vasylchenko/Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_Task_3_Vasylchenko
+{
+    public class Class2
+    {
+        private readonly Class1 _ob1 = new Class1();
+        private int _resut;
+
+        public Predicate<int> Calc(Func<int, int, int> z, int x, int y)
+        {
+            z = _ob1.Pow;
+            _resut = z(x, y);
+            Predicate<int> delegatResult = Result;
+            return delegatResult;
+        }
+
+        public bool Result(int x)
+        {
+            if (x % _resut == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Module_3_Task_3_Vasylchenko/Module_3_Task_3_Vasylchenko/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module_3_Task_3_Vasylchenko
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new Class1();
+            var b = new Class2();
+            a.ShowHelder = Show;
+            a.ShowHelder(b.Calc(a.PowDelegat, 2, 3).Invoke(2));
+        }
+
+        public static void Show(bool b)
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
